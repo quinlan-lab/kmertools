1100d3637893d11ea96fa4971edf475dfdd8d748
Simone Longo <simone.longo@gmail.com>
2019-11-21 16:13:57 -0700

    Added command line args to test.py

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
 import eskedit as ek
-#vcf_path = '/Users/simonelongo/too_big_for_icloud/gnomAD_v3/gnomad.genomes.r3.0.sites.vcf.bgz'
-#fasta_path = '/Users/simonelongo/too_big_for_icloud/ref_genome/hg38/hg38.fa'
+import sys
+
+# vcf_path = '/Users/simonelongo/too_big_for_icloud/gnomAD_v3/gnomad.genomes.r3.0.sites.vcf.bgz'
+# fasta_path = '/Users/simonelongo/too_big_for_icloud/ref_genome/hg38/hg38.fa'
 
 vcf_path = '/scratch/general/lustre/u0319040/gnomadv3/gnomad.genomes.r3.0.sites.vcf.bgz'
 fasta_path = '/scratch/general/lustre/u0319040/ref_genome/hg38/hg38.fa'
 
-results = ek.get_kmer_context(vcf_path, fasta_path, 3, nprocs=6)
+try:
+    ksize = int(sys.argv[1])
+except (ValueError, IndexError):
+    ksize = 3
+
+try:
+    n_threads = int(sys.argv[2])
+except (ValueError, IndexError):
+    n_threads = 6
+
+results = ek.get_kmer_context(vcf_path, fasta_path, ksize, nprocs=n_threads)
 # results should be  list of dictionaries
 transitions = []
 positions = []
@@ -19,7 +31,7 @@ for coll in results:
 # s_transitions = defaultdict(Counter)
 # s_positions = defaultdict(Variant)
 ek.merge_positions_dd(positions, outfile='singletons_test.bed')
-ek.merge_transitions_ddc(transitions)
+ek.merge_transitions_ddc(transitions, outfile='bp_counts_3mer.csv')
 
 # /scratch/general/lustre/u0319040/gnomadv3/gnomad.genomes.r3.0.sites.vcf.bgz
 # /scratch/general/lustre/u0319040/ref_genome/hg38/hg38.fa
