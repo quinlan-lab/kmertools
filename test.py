b209f82d87cc734da4d0609e0cb31b9afa83ec8c
Simone Longo <simone.longo@gmail.com>
2019-11-21 16:22:58 -0700

    Added more command line args to test.py and fixed bug with multiprocessing

--- test.py
+++ test.py
@@ -17,6 +17,12 @@ try:
 except (ValueError, IndexError):
     n_threads = 6
 
+if '-save-vars' in sys.argv:
+    var_out = 'singletons_test.bed'
+else:
+    var_out = None
+
+t_fpath = 'counts_' + str(ksize) + 'mer.bed'
 results = ek.get_kmer_context(vcf_path, fasta_path, ksize, nprocs=n_threads)
 # results should be  list of dictionaries
 transitions = []
@@ -30,8 +36,8 @@ for coll in results:
 
 # s_transitions = defaultdict(Counter)
 # s_positions = defaultdict(Variant)
-ek.merge_positions_dd(positions, outfile='singletons_test.bed')
-ek.merge_transitions_ddc(transitions, outfile='bp_counts_3mer.csv')
+ek.merge_positions_dd(positions, outfile=var_out)
+ek.merge_transitions_ddc(transitions, outfile=t_fpath)
 
 # /scratch/general/lustre/u0319040/gnomadv3/gnomad.genomes.r3.0.sites.vcf.bgz
 # /scratch/general/lustre/u0319040/ref_genome/hg38/hg38.fa
